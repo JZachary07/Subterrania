f2e6044de3990a55d9869ad6667425622bc0bec5
Zachary <JZachary8696@gmail.com>
2020-02-17 15:32:27 -0500

    added controller framework

---
+++ CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Godot;
+
+namespace Subterrania.Core
+{
+	public class CameraController : Camera
+	{
+		public TerrainController()
+		{
+
+		}
+
+		public override void _Ready()
+		{
+
+		}
+
+		public override void _Process(float delta)
+		{
+
+		}
+	}
+}
---
+++ TerrainController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Godot;
+
+namespace Subterrania.Core.Terrain
+{
+	public class TerrainController : Object
+	{
+		public TerrainController()
+		{
+
+		}
+
+		public override void _Ready()
+		{
+
+		}
+
+		public override void _Process(float delta)
+		{
+
+		}
+	}
+}
